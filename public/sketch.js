1af4b4f81d2bae65c2ca76c2d8ffdfdf3d93e2d8
Siunami <Siunami.matt@gmail.com>
2019-09-14 15:09:10 -0700

    handle adding trees

--- public/sketch.js
+++ public/sketch.js
@@ -6,6 +6,10 @@ let imageTree1;
 let imageTree1Width = 50;
 let imageTree1Height = 67;
 
+let imageTree2;
+let imageTree2Width = 50;
+let imageTree2Height = 72;
+
 let WIDTH = 750;
 let HEIGHT = 500
 
@@ -20,34 +24,41 @@ const placedState = {
 }
 
 const treeType = {
+    'NONE':'none',
     'TREE1': 'tree1',
     'TREE2': 'tree2'
 }
 
-let currentState;
+let currentSelectState;
+let currentPlacedState;
 let currentTree;
+
+let tempTreeList = [];
 
 function preload(){
     imageTree1 = loadImage('test_tree.png');
+    imageTree2 = loadImage('test_tree2.png');
 }
 
 function setup() {
-    currentState = selectedState.NONE_SELECTED;
+    currentSelectState = selectedState.NONE_SELECTED;
+    currentPlacedState = placedState.NONE_PLACED;
+    currentTree = treeType.NONE;
 
     tree1 = createButton("Tree1");
     tree1.class("myTree")
     tree1.parent("tree1")
     tree1.attribute("value","tree1")
     tree1.mouseClicked(function(){ 
-        currentState = selectedState.SELECTED;
+        currentSelectState = selectedState.SELECTED;
         currentTree = treeType.TREE1;
     })
     tree2 = createButton("Tree2");
     tree2.class("myTree")
-    tree2.parent("tree1")
+    tree2.parent("tree2")
     tree2.attribute("value","tree2")
     tree2.mouseClicked(function(){ 
-        currentState = selectedState.SELECTED;
+        currentSelectState = selectedState.SELECTED;
         currentTree = treeType.TREE2;
     })
     ////////////
@@ -58,16 +69,32 @@ function setup() {
     var canvas = createCanvas(WIDTH,HEIGHT);
     canvas.class('canvas');
     canvas.parent('canvas-holder');
+    canvas.mouseClicked(mouseHandle)
     background(51);
     ////////////
 
     //   socket = io.connect('http://localhost:3000');
 }
 
-function mouseClicked(){
-    if (currentState == selectedState.SELECTED){
+function mouseHandle(){
+    if (currentSelectState == selectedState.SELECTED){
+        console.log([currentSelectState,currentTree])
         // TODO: Bounding box avoid too much tree overlap algorithm
-        image(imageTree1, mouseX - imageTree1Width/2, mouseY - imageTree1Height/2)
+        if (currentTree == treeType.TREE1){
+            image(imageTree1, mouseX - imageTree1Width/2, mouseY - imageTree1Height/2)
+        } else if (currentTree == treeType.TREE2) {
+            image(imageTree2, mouseX - imageTree2Width/2, mouseY - imageTree2Height/2)
+        }
+
+        tempTreeList.push({
+            "x": mouseX,
+            "y": mouseY,
+            "treetype": currentTree
+        })
+
+        currentSelectState = selectedState.NONE_SELECTED;
+        currentPlacedState = placedState.TEMP_PLACED;
+        currentTree = treeType.NONE;
 
     }
     // for (let i = 0; i < 100; i++){
