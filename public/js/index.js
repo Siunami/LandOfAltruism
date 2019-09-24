14dd70c2b90ee5cc67920f1fac480724138d6bda
Siunami <Siunami.matt@gmail.com>
2019-09-24 13:58:00 -0700

    nav commented out

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,5 @@
 // Get the modal
-var modal = document.getElementById("myModal");
+var modal = document.getElementsByClassName("modal-content")[0];
 
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn");
@@ -9,17 +9,19 @@ var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
-  modal.style.display = "block";
+  console.log(modal);
+  console.log("asdfs")
+  modal.style.visibility = "visible";
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-  modal.style.display = "none";
+  modal.style.visibility = "hidden";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
-    modal.style.display = "none";
+    modal.style.visibility = "hidden";
   }
 }
